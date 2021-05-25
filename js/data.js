a42a6bb4cac83dea74c0b31bbc334ff1ab71e824
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-24 21:04:59 -0700

    Verify that new entries are saved to local storage along with the rest of the data model on page reload. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,8 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  var newEntryJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', newEntryJSON);
+});
