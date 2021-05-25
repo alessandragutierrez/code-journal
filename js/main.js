92ed73d5f1fc0d66026aad4da1599daa9a9b83c4
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-24 17:24:14 -0700

    Listen for submit events on the journal entry form.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@
 
 var photoUrl = document.querySelector('.photoUrl');
 var photo = document.querySelector('img');
+var entryForm = document.querySelector('.form');
 
 photoUrl.addEventListener('input', updatePhoto);
 
 function updatePhoto(event) {
   photo.setAttribute('src', photoUrl.value);
+}
+
+entryForm.addEventListener('submit', saveEntryValues);
+
+function saveEntryValues(event) {
+
 }
